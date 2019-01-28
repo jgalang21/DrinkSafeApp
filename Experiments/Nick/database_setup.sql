fb909a4a27efbcc3e251f282e14da6d76b396425
Nick Rosenstiel <njrosenstiel@gmail.com>
2019-01-28 13:06:07 -0600

    sql_database

---
+++ Experiments/Nick/database_setup.sql
@@ -0,0 +1,7 @@
+create  database if not exists `petclinic`;
+
+/*
+INSERT IGNORE INTO owners VALUES (NULL, '110 W. Liberty St.', 'George', 'Franklin', '6085551023');
+INSERT IGNORE INTO owners VALUES (NULL, '638 Cardinal Ave.', 'Betty', 'Davis', '6085551749');
+INSERT IGNORE INTO owners VALUES (NULL, '2693 Commerce St.', 'Eduardo', 'Rodriquez', '6085558763');
+*/
