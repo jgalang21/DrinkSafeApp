81b65afe753c71cf557541b78f5b81df72644725
Jeremy <jjgalang@iastate.edu>
2019-02-14 12:22:11 -0600

    Backend server

---
+++ Backend/DrinkSafe/DrinkSafeServer/src/main/java/com/example/demo/DrinkSafeServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkSafeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkSafeServerApplication.class, args);
+	}
+
+}
+
