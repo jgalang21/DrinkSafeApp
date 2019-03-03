c2cad2c9eb677e1979ce4f5ab9807407da100ede
jdlee <jdlee@iastate.edu>
2019-03-03 16:40:44 -0600

    set up sign in will get better later

--- Frontend/Login/app/src/main/java/com/jorden/login/MainActivity.java
+++ Frontend/Login/app/src/main/java/com/jorden/login/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jorden.login;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,22 +10,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.jorden.app.AppController;
 import com.jorden.net_utils.Const;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView msgResponse;
     private EditText Email;
     private EditText Password;
     private TextView Info;
     private Button Login;
     private int counter = 5;
+    private ProgressDialog pDialog;
+    private String TAG = SecondActivity.class.getSimpleName();
+    private String tag_json_obj = "jobj_req", tag_json_arry = "jarray_req";
 
 
     @Override
@@ -38,28 +51,93 @@ public class MainActivity extends AppCompatActivity {
         Login = (Button) findViewById(R.id.btnLogin);
 
         Info.setText("attempts left: 5");
-        Login.setOnClickListener(new View.OnClickListener(){
-           @Override
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
                 validate(Email.getText().toString(), Password.getText().toString());
-           }
+            }
         });
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
     }
 
-    private void validate(String userEmail, String userPass){
-        if((userEmail.equals("Admin")) && (userPass.equals("123Abc"))){
+    private void validate(String userEmail, String userPass) {
+        if ((userEmail.equals("Admin")) && (userPass.equals("123Abc"))) {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
             startActivity(intent);
 
-        }else{
+        } else {
             counter--;
             Info.setText("Attempts left: " + String.valueOf(counter));
-            if(counter == 0){
+            if (counter == 0) {
                 counter = 0;
                 Login.setEnabled(false);
             }
         }
     }
+
+    private void showProgressDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (pDialog.isShowing())
+            pDialog.hide();
+    }
+
+    private void makeJsonObjReq() {
+        showProgressDialog();
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                Const.URL_USER_INFO, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        msgResponse.setText(response.toString());
+                        hideProgressDialog();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hideProgressDialog();
+            }
+        }) {
+
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", "Androidhive");
+                params.put("email", "abc@androidhive.info");
+                params.put("pass", "password123");
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq,
+                tag_json_obj);
+
+        // Cancelling request
+        // ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_obj);
+    }
+
     private void makeJsonArryReq() {
         showProgressDialog();
         JsonArrayRequest req = new JsonArrayRequest(Const.URL_USER_INFO/*URL_USER_INFO*/,
@@ -79,4 +157,5 @@ public class MainActivity extends AppCompatActivity {
                 hideProgressDialog();
             }
         });
+    }
 }
