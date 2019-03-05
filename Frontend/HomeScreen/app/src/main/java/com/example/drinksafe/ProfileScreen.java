0d50f7486ce1cc2569ac9a4d5272874c387ec41e
paynep97 <paynep97@iastate.edu>
2019-03-05 12:33:20 -0600

    Getting close to done with profile screen

--- Frontend/HomeScreen/app/src/main/java/com/example/drinksafe/ProfileScreen.java
+++ Frontend/HomeScreen/app/src/main/java/com/example/drinksafe/ProfileScreen.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class ProfileScreen extends AppCompatActivity {
     EditText name_box, phone_box, email_box;
+    Spinner sex_s, feet_s, inches_s, lbs_s;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,29 @@ public class ProfileScreen extends AppCompatActivity {
         name_box.setEnabled(false);
         phone_box.setEnabled(false);
         email_box.setEnabled(false);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.sex_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        sex_s.setAdapter(adapter1);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.height_feet_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        feet_s.setAdapter(adapter2);
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.height_inches_array, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        inches_s.setAdapter(adapter3);
+
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                R.array.height_feet_array, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lbs_s.setAdapter(adapter4);
 
         ImageButton b_back = findViewById(R.id.back_button);
         b_back.setOnClickListener(new View.OnClickListener() {
