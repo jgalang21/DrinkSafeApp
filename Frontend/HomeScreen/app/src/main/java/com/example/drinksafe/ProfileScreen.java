01cca394a54cc53019e02c37154f54e92db12fff
paynep97 <paynep97@iastate.edu>
2019-03-07 12:31:53 -0600

    Wrote some more code for the profile screen

--- Frontend/HomeScreen/app/src/main/java/com/example/drinksafe/ProfileScreen.java
+++ Frontend/HomeScreen/app/src/main/java/com/example/drinksafe/ProfileScreen.java
@@ -13,9 +13,11 @@ import com.example.drinksafe.net_utils.Const;
 import com.example.drinksafe.app.AppController;
 
 public class ProfileScreen extends AppCompatActivity {
-    private EditText name_box, phone_box, email_box;
-    private Spinner sex_s, feet_s, inches_s, lbs_s;
+    private EditText name_box, weight_box, email_box;
+    private Spinner gender_s, feet_s, inches_s;
     private String name, email;
+    private int weight, height, gender;
+    private int[] height_arr = new int[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +25,16 @@ public class ProfileScreen extends AppCompatActivity {
         setContentView(R.layout.activity_profile_screen);
 
         name_box = findViewById(R.id.name_edit_box);
-        phone_box = findViewById(R.id.phone_edit_box);
+        weight_box = findViewById(R.id.weight_edit_box);
         email_box = findViewById(R.id.email_edit_box);
 
         name_box.setEnabled(false);
-        phone_box.setEnabled(false);
+        weight_box.setEnabled(false);
         email_box.setEnabled(false);
 
-        sex_s = (Spinner) findViewById(R.id.sex_spin);
+        gender_s = (Spinner) findViewById(R.id.sex_spin);
         feet_s = (Spinner) findViewById(R.id.feet_spin);
         inches_s = (Spinner) findViewById(R.id.incehs_spin);
-        //lbs_s = (Spinner) findViewById(R.id.spinner);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
@@ -41,7 +42,7 @@ public class ProfileScreen extends AppCompatActivity {
         // Specify the layout to use when the list of choices appears
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        sex_s.setAdapter(adapter1);
+        gender_s.setAdapter(adapter1);
 
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
                 R.array.height_feet_array, android.R.layout.simple_spinner_item);
@@ -53,12 +54,7 @@ public class ProfileScreen extends AppCompatActivity {
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         inches_s.setAdapter(adapter3);
 
-        /*ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
-                R.array.height_feet_array, android.R.layout.simple_spinner_item);
-        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        lbs_s.setAdapter(adapter4);*/
-
-        sex_s.setEnabled(false);
+        gender_s.setEnabled(false);
         feet_s.setEnabled(false);
         inches_s.setEnabled(false);
 
@@ -81,23 +77,49 @@ public class ProfileScreen extends AppCompatActivity {
                 if(status == 1) {
                     b.setText(R.string.save_t);
                     name_box.setEnabled(true);
-                    phone_box.setEnabled(true);
+                    weight_box.setEnabled(true);
                     email_box.setEnabled(true);
-                    sex_s.setEnabled(true);
+                    gender_s.setEnabled(true);
                     feet_s.setEnabled(true);
                     inches_s.setEnabled(true);
                     v.setTag(0);
                 } else {
                     b.setText(R.string.edit_t);
                     name_box.setEnabled(false);
-                    phone_box.setEnabled(false);
+                    weight_box.setEnabled(false);
                     email_box.setEnabled(false);
-                    sex_s.setEnabled(false);
+                    gender_s.setEnabled(false);
                     feet_s.setEnabled(false);
                     inches_s.setEnabled(false);
                     v.setTag(1);
                 }
             }
         });
+
+        getInfo(this.name, this.email, this.weight, this.height, this.gender);
+
+        heightConv(this.height, this.height_arr, true);
+    }
+
+    private void getInfo(String name, String email, int weight, int height, int gender) {
+
+    }
+
+    /**
+     * Simple helper method to convert the height measurement from inches to feet and inches
+     * @param h height in inches
+     * @param h_arr array used to store height
+     * @param convert boolean to indicate which way to convert
+     */
+    private void heightConv(int h, int[] h_arr, boolean convert) {
+        if(convert) {
+            h_arr[0] = h / 12;
+            h_arr[1] = h % 12;
+        } else {
+            int temp = 0;
+            temp += h_arr[0] * 12;
+            temp += h_arr[1];
+            h = temp;
+        }
     }
 }
