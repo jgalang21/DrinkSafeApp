2b5bc05ec57682fad3872fe4986cbb4cadc18f72
Nick Rosenstiel <njrosenstiel@gmail.com>
2019-04-02 12:06:58 -0500

    groups

--- Backend/DrinkSafe/DrinkSafeServer/src/main/java/org/springframework/samples/drink_safe/user/Group.java
+++ Backend/DrinkSafe/DrinkSafeServer/src/main/java/org/springframework/samples/drink_safe/user/Group.java
@@ -1,5 +1,22 @@
 package org.springframework.samples.drink_safe.user;
 
-public class Group {
+import java.util.HashSet;
 
+public class Group {
+	
+	HashSet<User> group = new HashSet<User>();
+
+	public String getGroup() {
+		String returner = "";
+		for(User u: group)
+			returner+= u.getUsername()+ " ";
+		return returner;
+	}
+
+	public void setGroup(HashSet<User> group) {
+		this.group = group;
+	}
+	
+
+	
 }
