27443baaf1b62778ce06d39128f9f95e33a3f481
Nick Rosenstiel <njrosenstiel@gmail.com>
2019-04-04 15:29:27 -0500

    grouping

--- Backend/DrinkSafe/DrinkSafeServer/src/main/java/org/springframework/samples/drink_safe/user/Group.java
+++ Backend/DrinkSafe/DrinkSafeServer/src/main/java/org/springframework/samples/drink_safe/user/Group.java
@@ -2,9 +2,35 @@ package org.springframework.samples.drink_safe.user;
 
 import java.util.HashSet;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name="group")
 public class Group {
+	@Id
+    @Column(name = "gm")
+    @NotFound(action = NotFoundAction.IGNORE)
+	private String gm;
 	
-	HashSet<User> group = new HashSet<User>();
+    
+    @Column(name = "m")
+    @NotFound(action = NotFoundAction.IGNORE)
+	private String m;
+    public Group()
+    {
+    	
+    }
+    public Group(String gm, String ,)
+    {
+    	this.gm=u1;
+    	sentto=u2;
+    }
 
 	public String getGroup() {
 		String returner = "";
