59fb5417886c69b4ee62f04d366085f0ae4107cc
Jeremy <jjgalang@iastate.edu>
2019-04-05 13:24:38 -0500

    Finally done with tests

--- Backend/DrinkSafe/DrinkSafeServer/src/test/java/org/springframework/samples/drink_safe/tests/TestUser.java
+++ Backend/DrinkSafe/DrinkSafeServer/src/test/java/org/springframework/samples/drink_safe/tests/TestUser.java
@@ -64,7 +64,6 @@ public class TestUser {
 		//public User(String username, String name, String password, int height, int weight,int gender, int guestStatus)
 		User x = new User("jman22", "John", "Dogs", 48, 222, 0, 0);
 		
-		
 		assertEquals("jman22", x.getUsername());
 		assertEquals("John", x.getName());
 		assertEquals("Dogs", x.getPassword());
@@ -155,17 +154,85 @@ public class TestUser {
 		assertEquals(10, w.getAlcpercent());
 		assertEquals(23, w.getVolume());
 		assertEquals("lmaokai", w.getFkuser());
-		
-		
+			
 	}
 	
 	
+	//Give one drink
+	@Test
+	public void countDrinks() {
+		User r = mock(User.class);
+		Drink w = mock(Drink.class);
+		
+		r.giveDrink(w);
+		
+		//when(r.getGuestStatus()).thenReturn(1);
+		
+		verify(r, times(1)).giveDrink(w);
+		
+		
+
+	}
+	
+	//Counting multiple instances of one drink
+	@Test
+	public void countDrinks2() {
+		User r = mock(User.class);
+		Drink w = mock(Drink.class);
+		
+		r.giveDrink(w);
+		r.giveDrink(w);
+		r.giveDrink(w);
+		r.giveDrink(w);
+		r.giveDrink(w);
+		r.giveDrink(w);
+		
+		verify(r, times(6)).giveDrink(w);
+		
+
+	}
+	
+	//Count the number of drinks
+	@Test
+	public void countDrinks3() {
+		User r = mock(User.class);
+		Drink w = mock(Drink.class);
+		Drink k = mock(Drink.class);
+		Drink l = mock(Drink.class);
+		
+		r.giveDrink(w);
+		r.giveDrink(w);
+		r.giveDrink(w);
+		r.giveDrink(w);
+		r.giveDrink(w);
+		r.giveDrink(w);
+		r.giveDrink(k);
+		r.giveDrink(k);
+		r.giveDrink(l);
+		
+		verify(r, times(6)).giveDrink(w);
+		verify(r, times(2)).giveDrink(k);
+		verify(r, times(1)).giveDrink(l);
+		
+
+	}
+	
+	@Test
+	public void UserCont() {
+		
+		User r = new User("jman22", "Charles", "Dogs", 48, 222, 0, 0);
+		UserController k = mock(UserController.class);
+		
+		when(k.findUserbyID("Charles")).thenReturn(r);
+		
+		
+		assertEquals(r, k.findUserbyID("Charles"));
+		
+		
+	}	
 	
 	
-	
-	
-	
-	//// --------------------------------------------------JEREMY'S TESTS ---------------------------
+	//--------------------------------------------------JEREMY'S TESTS ---------------------------
 	
 	
 	
@@ -183,4 +250,8 @@ public class TestUser {
 
 
 	}
+	
+	
+
+	
 }
