c3266b13ab79f7eed30582a11cf55bfbf3a06575
jdlee <jdlee@iastate.edu>
2019-04-06 15:56:16 -0500

    messaging is thriving

---
+++ Frontend/HomeScreen/app/src/main/java/com/example/drinksafe/Messaging.java
@@ -0,0 +1,12 @@
+package com.example.drinksafe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Messaging extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+    }
+}
