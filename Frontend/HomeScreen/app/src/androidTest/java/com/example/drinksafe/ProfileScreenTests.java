de344fb52b53bf2c865471ae342ac00eb5710882
paynep97 <paynep97@iastate.edu>
2019-04-07 15:36:25 -0500

    Wrote some tests for the profile screen.  Also, finished writing the majority of code to push info to the server (still need to work out bugs)

--- Frontend/HomeScreen/app/src/androidTest/java/com/example/drinksafe/ProfileScreenTests.java
+++ Frontend/HomeScreen/app/src/androidTest/java/com/example/drinksafe/ProfileScreenTests.java
@@ -8,10 +8,13 @@ import org.junit.runner.RunWith;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.espresso.Espresso.*;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.assertion.ViewAssertions.*;
-import android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class ProfileScreenTests {
@@ -25,5 +28,32 @@ public class ProfileScreenTests {
 
 
     }
+
+    @Test
+    public void checkFieldStatesOnStart() {
+        onView(withId(R.id.name_edit_box)).check(matches(not(isEnabled())));
+        onView(withId(R.id.email_edit_box)).check(matches(not(isEnabled())));
+        onView(withId(R.id.weight_edit_box)).check(matches(not(isEnabled())));
+        onView(withId(R.id.inches_spin)).check(matches(not(isEnabled())));
+        onView(withId(R.id.feet_spin)).check(matches(not(isEnabled())));
+        onView(withId(R.id.sex_spin)).check(matches(not(isEnabled())));
+
+        onView(withId(R.id.save_edit_button)).check(matches(withText("Edit")));
+    }
+
+    @Test
+    public void checkFieldStatesOnClick() {
+        onView(withId(R.id.save_edit_button)).perform(click());
+
+        onView(withId(R.id.name_edit_box)).check(matches(isEnabled()));
+        onView(withId(R.id.email_edit_box)).check(matches(isEnabled()));
+        onView(withId(R.id.weight_edit_box)).check(matches(isEnabled()));
+        onView(withId(R.id.inches_spin)).check(matches(isEnabled()));
+        onView(withId(R.id.feet_spin)).check(matches(isEnabled()));
+        onView(withId(R.id.sex_spin)).check(matches(isEnabled()));
+
+        onView(withId(R.id.save_edit_button)).check(matches(withText("Edit")));
+    }
+
 
 }
