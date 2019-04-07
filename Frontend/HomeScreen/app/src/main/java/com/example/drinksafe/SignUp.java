e36d91688c1b6a9023335b9e2fd4ec66518175ad
jdlee <jdlee@iastate.edu>
2019-04-07 14:35:21 -0500

    working on pushing

--- Frontend/HomeScreen/app/src/main/java/com/example/drinksafe/SignUp.java
+++ Frontend/HomeScreen/app/src/main/java/com/example/drinksafe/SignUp.java
@@ -2,12 +2,39 @@ package com.example.drinksafe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
+    private EditText email, password, weight, heightIn, heightFt;
+    private Switch maleSw, femSw;
+    private Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        email = (EditText) findViewById(R.id.userEmail);
+        password = (EditText) findViewById(R.id.UserPass);
+        weight = (EditText) findViewById(R.id.userWeight);
+        heightIn = (EditText) findViewById(R.id.height_in);
+        heightFt = (EditText) findViewById(R.id.height_ft);
+        maleSw = (Switch) findViewById(R.id.maleSex);
+        femSw = (Switch) findViewById(R.id.femSex);
+        signUp = (Button) findViewById(R.id.SignUpbtn);
+    }
+    //height conversion from ProfileScreen
+    private void heightConv(int h, int[] h_arr, boolean convert) {
+        if(convert) {
+            h_arr[0] = h / 12;
+            h_arr[1] = h % 12;
+        } else {
+            int temp = 0;
+            temp += h_arr[0] * 12;
+            temp += h_arr[1];
+            h = temp;
+        }
     }
 }
