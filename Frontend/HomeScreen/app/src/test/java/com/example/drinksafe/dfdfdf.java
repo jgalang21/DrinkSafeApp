21285a91154fe6f4e695d85040de3f8036681fd3
Jeremy <jjgalang@iastate.edu>
2019-04-10 17:07:06 -0500

    Adding to personal github

--- Frontend/HomeScreen/app/src/test/java/com/example/drinksafe/dfdfdf.java
+++ Frontend/HomeScreen/app/src/test/java/com/example/drinksafe/dfdfdf.java
@@ -10,7 +10,7 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
-public class SignInTest {
+public class dfdfdf {
     @Mock
     SignIn mockSignin;
 
