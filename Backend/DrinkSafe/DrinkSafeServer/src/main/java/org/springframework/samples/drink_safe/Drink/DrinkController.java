bb8e6e2497bdfd395061c62385ce227d2bbe1e79
Jeremy <jjgalang@iastate.edu>
2019-04-11 12:21:59 -0500

    pulling

--- Backend/DrinkSafe/DrinkSafeServer/src/main/java/org/springframework/samples/drink_safe/Drink/DrinkController.java
+++ Backend/DrinkSafe/DrinkSafeServer/src/main/java/org/springframework/samples/drink_safe/Drink/DrinkController.java
@@ -1,6 +1,9 @@
 package org.springframework.samples.drink_safe.Drink;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,53 +17,69 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-	public class DrinkController {
+public class DrinkController {
 	private final Logger logger = LoggerFactory.getLogger(DrinkController.class);
-	
+
 	@Autowired
 	DrinkRepository drinkRepo;
-	
+
 	@Autowired
 	UserRepository userRepo;
-	
 
-	
-	@RequestMapping(method = RequestMethod.GET, path= "/drink/new/{DrinkId}/{alcPercent}/{volume}/{fkuser}")
-	public void addDrink(@PathVariable("DrinkId") String DrinkId,@PathVariable("alcPercent") int alcPercent,@PathVariable("volume") int volume, @PathVariable("fkuser") String fkuser)
-	{
+	@RequestMapping(method = RequestMethod.GET, path = "/drink/new/{DrinkId}/{alcPercent}/{volume}/{fkuser}")
+	public void addDrink(@PathVariable("DrinkId") String DrinkId, @PathVariable("alcPercent") int alcPercent,
+			@PathVariable("volume") int volume, @PathVariable("fkuser") String fkuser) {
 		long x = System.nanoTime();
 		User u = userRepo.findByUsername(fkuser);
-		time t = new time(u.getUsername().hashCode(),x,x+1000);
+		time t = new time(u.getUsername().hashCode(), x, x + 1000);
 		u.setUser_time(t);
 		userRepo.save(u);
 		List<Drink> r = (List<Drink>) drinkRepo.findAll();
 		int did;
-		if(r.isEmpty())
-			did=0;
+		if (r.isEmpty())
+			did = 0;
 		else
 			did = r.size();
-		Drink drink = new Drink(did,DrinkId, alcPercent, volume,u);
+		Drink drink = new Drink(did, DrinkId, alcPercent, volume, u);
 		drinkRepo.save(drink);
-		logger.info(fkuser + " had added " +DrinkId + " as a drink");
+		logger.info(fkuser + " had added " + DrinkId + " as a drink");
 	}
-	
 
-	
-	
-	@RequestMapping(method = RequestMethod.GET, path ="/drink")
-	public List<Drink> listAllDrinks(){
+	@RequestMapping(method = RequestMethod.GET, path = "/drink")
+	public List<Drink> listAllDrinks() {
 		logger.info("Entered into control layer (For drinks)");
 		List<Drink> r = (List<Drink>) drinkRepo.findAll();
 		logger.info("displaying all drinks");
 		return r;
+
+	}
+
+	public double calculateBAC(@PathVariable("username") User username, @PathVariable("hours") double time) {
+		double height = username.getHeight();
+		double weight = username.getWeight();
+		int gender = username.getGender();
+		double ratio = 0.0;
+
+		if (gender == 1) { // constant ratio for women
+			ratio = 0.66;
+		} else {
+			ratio = 0.73;
+		} // constant ratio for men
+
+		double temp = 0.0;
+
+		String drinkList = username.getDrinks(); // all of the user's drinks
+		String[] drinkArr; 
 		
+		drinkArr = drinkList.split(" ");
+		
+		
+		for(int i = 0; i < drinkArr.length; i++) {
+			
+		}
+		
+		
+		return 0;
 	}
 
-	
-	}
-
-
-// PRIMARY KEY (`drinkId`),
-//CONSTRAINT `cusername` FOREIGN KEY (`cusername`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE);
-
-
+}
