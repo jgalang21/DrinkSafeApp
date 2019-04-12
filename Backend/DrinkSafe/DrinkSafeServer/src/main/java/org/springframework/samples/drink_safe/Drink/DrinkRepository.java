8a9a58c8347151ed8b1a8e92fd761d006e06412c
Jeremy <jjgalang@iastate.edu>
2019-04-12 14:29:24 -0500

    Calculations

--- Backend/DrinkSafe/DrinkSafeServer/src/main/java/org/springframework/samples/drink_safe/Drink/DrinkRepository.java
+++ Backend/DrinkSafe/DrinkSafeServer/src/main/java/org/springframework/samples/drink_safe/Drink/DrinkRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DrinkRepository extends CrudRepository<Drink, Long> {
 
+	Drink findByDrinkId(String drinkId);
+
 
 
 
