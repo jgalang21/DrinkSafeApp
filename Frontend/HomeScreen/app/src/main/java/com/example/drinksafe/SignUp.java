c68ef489ef1b7fe02172a8debc2fbf8e32cc5ff0
jdlee <jdlee@iastate.edu>
2019-04-16 12:00:58 -0500

    getting Sign Up working

--- Frontend/HomeScreen/app/src/main/java/com/example/drinksafe/SignUp.java
+++ Frontend/HomeScreen/app/src/main/java/com/example/drinksafe/SignUp.java
@@ -1,19 +1,25 @@
 package com.example.drinksafe;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.drinksafe.app.AppController;
+import com.example.drinksafe.net_utils.Const;
 
 import org.json.JSONArray;
 
@@ -23,12 +29,13 @@ import static com.example.drinksafe.net_utils.Const.URL_USER_INFO;
 public class SignUp extends AppCompatActivity {
 
     private EditText email, password, weight, heightIn, heightFt;
-    private Switch sexSw;
+    private CheckBox sexF,sexM;
     private Button signUp;
     private ProgressDialog pDialog;
     private String TAG = SignIn.class.getSimpleName();
     private int height, sex;
     private String tempURL;
+    private boolean switchStatus, mayProceed = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,17 +45,37 @@ public class SignUp extends AppCompatActivity {
         weight = (EditText) findViewById(R.id.userWeight);
         heightIn = (EditText) findViewById(R.id.height_in);
         heightFt = (EditText) findViewById(R.id.height_ft);
-        sexSw = (Switch) findViewById(R.id.mfSwitch);
+        sexM = (CheckBox) findViewById(R.id.checkBoxM);
+        sexF = (CheckBox) findViewById(R.id.checkBoxF);
         signUp = (Button) findViewById(R.id.SignUpbtn);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
-                    height = (Integer.parseInt(heightFt.getText().toString())*12)+Integer.parseInt(heightIn.getText().toString());
-                    tempURL = URL_USER_INFO;
-                    tempURL += "/new/" + email +"/";
-                    makeJsonArrayReq();
+                    height = (Integer.parseInt(heightFt.getText().toString()) * 12) + Integer.parseInt(heightIn.getText().toString());
+                    Boolean maleStatus = sexM.isChecked();
+                    Boolean femaleStatus = sexF.isChecked();
+                    if (maleStatus == false && femaleStatus == true){
+                        sex = 1;
+                        mayProceed = true;
+                    }
+                    else if (maleStatus == true && femaleStatus == false){
+                        sex = 0;
+                        mayProceed = true;
+                    }
+                    else if (maleStatus == false&&femaleStatus == false) {
+                        Toast.makeText(getApplicationContext(), "please identify a sex", Toast.LENGTH_LONG).show();
+                        mayProceed = false;
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "please identify as only one sex", Toast.LENGTH_LONG).show();
+                        mayProceed = false;
+                    }
+
+                    tempURL += "/height/" + Const.cur_user_name + "/" + height;
+                    JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, tmpURL, null, null, null);
+                    //makeJsonArrayReq();
                     System.err.println("makeJsonArrayReq");
                     System.err.println("Finish validate");
                 } catch (Exception e) {
@@ -79,6 +106,10 @@ public class SignUp extends AppCompatActivity {
                         public void onResponse(JSONArray response) {
 
                             hideProgressDialog();
+                            if(mayProceed){
+                                Intent intent = new Intent(SignUp.this, SignIn.class);
+                                startActivity(intent);
+                            }
 
                         }
                     }, new Response.ErrorListener() {
@@ -94,16 +125,5 @@ public class SignUp extends AppCompatActivity {
             AppController.getInstance().addToRequestQueue(req);
             System.err.println("Request to queue added");
         }
-            //height conversion from ProfileScreen
- /*   private void heightConv(int h, int[] h_arr, boolean convert) {
-        if(convert) {
-            h_arr[0] = h / 12;
-            h_arr[1] = h % 12;
-        } else {
-            int temp = 0;
-            temp += h_arr[0] * 12;
-            temp += h_arr[1];
-            h = temp;
-        }
-    }*/
+
 }
